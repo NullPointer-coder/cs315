e10fbbfa37311b9d1b2376cc27bce99584ff4cf0
Jingbo Wang <jw6347@vh222004.truman.edu>
2022-03-16 10:28:39 -0500

    new file:   316/managewords.js~ 	new file:   316/managewords.php 	new file:   316/managewords.phps new file:   316/parts.txt

---
+++ 316/managewords.js
@@ -0,0 +1,362 @@
+// Jingbo Wang
+
+"use strict";
+
+const $ = (id) => document.getElementById(id);
+
+/**
+ * always test words, part of speech, and definition
+ */
+window.onchange = function ()
+{
+  add_test();
+};
+
+/**
+ * click different chosen checkbox to show diffferent perform
+ */
+window.onload = function ()
+{
+  $("add-cb").onclick = click_add;
+  $("add-pos-cb").onclick = click_add_pos;
+  $("del-cb").onclick = click_del;
+};
+
+/**
+ * To add, test a legal words, part of speach and definition
+ */
+function click_add()
+{
+  let words_list = document.getElementsByClassName("wordslist");
+  $("del-cb").checked = false;
+  $("add-pos-cb").checked = false;
+  $("add-wrapper").classList.remove("invisible");
+  $("add-wrapper").classList.add("visible");
+  $("add-pos-wrapper").classList.remove("visible");
+  $("add-pos-wrapper").classList.add("invisible");
+  $("del-wrapper").classList.remove("visible");
+  $("del-wrapper").classList.add("invisible");
+
+  // clean
+  $("words").value = "";
+  $("partofspeech").value = "";
+  $("definition").value = "";
+  $("newspeech").value = "";
+  $("submit-success-statement").innerHTML = "";
+  let index = 0;
+  while (index < words_list.length)
+  {
+    words_list[index].checked = false;
+    index += 1;
+  }
+
+  // disable the submit button
+  $("add-submit").disabled = true;
+  $("add-pos-submit").disabled = true;
+  $("del-submit").disabled = true;
+
+  // add a new word
+  $("statement").style.color = "darkblue";
+  $("statement").innerHTML = "Add a new word you want!";
+  $("words").addEventListener("change", show_add_button);
+  $("partofspeech").addEventListener("change", show_add_button);
+  $("definition").addEventListener("change", show_add_button);
+
+  $("definition").addEventListener("change", definition_test);
+  $("words").addEventListener("change", word_test);
+}
+
+/**
+ * test the new word is valid or not
+ */
+function add_test()
+{
+
+  $("partofspeech").addEventListener("change", part_of_speech_test);
+
+}
+
+/**
+ * To add, test a new legal part of speach
+ */
+function click_add_pos()
+{
+  let words_list = document.getElementsByClassName("wordslist");
+  $("del-cb").checked = false;
+  $("add-cb").checked = false;
+  $("add-pos-wrapper").classList.remove("invisible");
+  $("add-pos-wrapper").classList.add("visible");
+  $("add-wrapper").classList.remove("visible");
+  $("add-wrapper").classList.add("invisible");
+  $("del-wrapper").classList.remove("visible");
+  $("del-wrapper").classList.add("invisible");
+
+  $("words").value = "";
+  $("partofspeech").value = "";
+  $("definition").value = "";
+  $("submit-success-statement").innerHTML = "";
+  let index = 0;
+  while (index < words_list.length)
+  {
+    words_list[index].checked = false;
+    index += 1;
+  }
+
+  $("add-submit").disabled = true;
+  $("add-pos-submit").disabled = true;
+  $("del-submit").disabled = true;
+
+  $("statement").style.color = "darkblue";
+  $("statement").innerHTML = "Add a new part of speech if it " +
+                                 "does not exist in the list and submit!";
+  $("newspeech").addEventListener("change", show_add_pos_button);
+
+  $("newspeech").addEventListener("change", new_pos_test);
+}
+
+/**
+ * To delete words in the dictionary
+ */
+function click_del()
+{
+  $("add-cb").checked = false;
+  $("add-pos-cb").checked = false;
+  $("del-wrapper").classList.remove("invisible");
+  $("del-wrapper").classList.add("visible");
+  $("add-pos-wrapper").classList.remove("visible");
+  $("add-pos-wrapper").classList.add("invisible");
+  $("add-wrapper").classList.remove("visible");
+  $("add-wrapper").classList.add("invisible");
+
+  $("add-submit").disabled = true;
+  $("add-pos-submit").disabled = true;
+  $("del-submit").disabled = true;
+
+  $("words").value = "";
+  $("partofspeech").value = "";
+  $("definition").value = "";
+  $("newspeech").value = "";
+  $("submit-success-statement").innerHTML = "";
+
+  $("statement").style.color = "darkblue";
+  $("statement").innerHTML = "Delete a word you want!";
+  $("word-list").addEventListener("change", show_del_button);
+}
+
+/**
+ * To show add-button when all checkboxs are valid and not null
+ */
+function show_add_button()
+{
+  if ($("words").value === ""
+      || $("partofspeech").value === ""
+      || $("definition").value === "")
+  {
+    $("add-submit").disabled = true;
+  }
+  else
+  {
+    $("add-submit").disabled = false;
+  }
+}
+
+/**
+ * To show add-pos-button when all elements is valid and not null
+ */
+function show_add_pos_button()
+{
+  if ($("newspeech").value === "")
+  {
+    $("add-pos-submit").disabled = true;
+  }
+  else
+  {
+    $("add-pos-submit").disabled = false;
+  }
+}
+
+/**
+ * To show del-button when checkbox is not null
+ */
+function show_del_button()
+{
+  let words_list = document.getElementsByClassName("wordslist");
+  let done = false;
+  let index = 0;
+  while(index < words_list.length && !done)
+  {
+    if(words_list[index].checked === true)
+    {
+      done = true;
+    }
+    index += 1;
+  }
+
+  if(!done)
+  {
+    $("del-submit").disabled = true;
+  }
+  else
+  {
+    $("del-submit").disabled = false;
+  }
+}
+
+/**
+ * To test word is valid or not
+ */
+function word_test()
+{
+  let lowwer_chars = /^[a-z]+$/;
+  if (!lowwer_chars.test($("words").value))
+  {
+    $("statement").style.color = "red";
+    $("statement").innerHTML = "Word has some ILLEGAL symbols!"
+                              + " Or upper case character! Try again!";
+    $("words").value = "";
+  }
+  else
+  {
+    $("statement").style.color = "darkblue";
+    $("statement").innerHTML = "";
+  }
+}
+
+/**
+ * To test wheather they have duplicate word and part of speech or not
+ */
+function part_of_speech_test()
+{
+  if ($("words").value === "")
+  {
+    $("statement").style.color = "red";
+    $("statement").innerHTML = "Add word first! Try again!";
+    $("words").value = "";
+    $("partofspeech").value = "";
+  }
+  else
+  {
+    $("statement").style.color = "darkblue";
+    $("statement").innerHTML = "";
+    let words_list = document.getElementsByClassName("wordslist");
+    let white_space = /\s/g;
+    let index = 0;
+    let done = false;
+    let new_word = $("words").value;
+    new_word = new_word.toLowerCase();
+    while (index < words_list.length && !done)
+    {
+      let word = $("id-"+index.toString()).innerText;
+      let row = word.split(":");
+      row[0] = row[0].replace(white_space,"");
+      row[1] = row[1].replace(white_space,"");
+      if (new_word === row[0] && $("partofspeech").value === row[1])
+      {
+        $("statement").style.color = "red";
+        $("statement").innerHTML = "Already have this word! Try again!";
+        $("words").value = "";
+        $("partofspeech").value = "";
+        done = true;
+      }
+      else
+      {
+        $("statement").style.color = "darkblue";
+        $("statement").innerHTML = "";
+      }
+      index += 1;
+    }
+  }
+}
+
+/**
+ * To test definition is valid or not 
+ */
+function definition_test()
+{
+  let white_space = /\s/g;
+  let printable_chars = /^[A-Za-z0-9!"#$%&'()*+,.\/:;<=>?@\[\] ^_`{|}~-]*$/i;
+  let string = $("definition").value;
+  string = string.replace("\n"," ");
+  let done = false;
+  let  index = 0;
+  if (string !== "")
+  {
+    while (index < string.length && !done)
+    {
+      if (white_space.test(string[index]))
+      {
+        index += 1;
+      }
+      else
+      {
+        if (!printable_chars.test(string[index]))
+        {
+          done = true;
+        }
+        index += 1;
+      }
+    }
+  }
+  else
+  {
+    done = true;
+  }
+  if (done)
+  {
+    $("statement").style.color = "red";
+    $("statement").innerHTML = "Definition has invalid characters! " +
+                                   "Try again!";
+    $("definition").value = "";
+  }
+  else
+  {
+    $("statement").style.color = "darkblue";
+    $("statement").innerHTML = "";
+  }
+}
+
+/**
+ * To test the new part of speech is valid or not
+ */
+function new_pos_test()
+{
+  let white_space = /\s/g;
+  let lowwer_chars = /^[a-z]+$/;
+  if (!lowwer_chars.test($("newspeech").value))
+  {
+    $("statement").style.color = "red";
+    $("statement").innerHTML = "New part of speech has some ILLEGAL symbols!"
+                               +" Or upper case character! Try again!";
+    $("newspeech").value = "";
+  }
+  else
+  {
+    $("statement").style.color = "darkblue";
+    $("statement").innerHTML = "";
+
+    let part_of_speech_list = $("partofspeech-list").innerText;
+    let part = part_of_speech_list.split(",");
+    let index = 0;
+    let done = false;
+    let new_pos = $("newspeech").value;
+    new_pos = new_pos.toLowerCase();
+    while (index < part.length && !done)
+    {
+      part[index] = part[index].replace(white_space,"");
+      if (new_pos === part[index])
+      {
+        $("statement").style.color = "red";
+        $("statement").innerHTML = "Already has this part of speech! "
+                                      + "Try again!";
+        $("newspeech").value = "";
+        done = true;
+      }
+      else
+      {
+        $("statement").style.color = "darkblue";
+        $("statement").innerHTML = "";
+      }
+      index += 1;
+    }
+  }
+}
---
+++ 316/managewords.php
@@ -0,0 +1,420 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+/**
+ * This program allows the user to add a new entry in the words file
+ * and to delete an existing entry.
+ * @author Jingbo Wang
+ * @version 13 February 2022
+ */
+define('DEFINITION_FILENAME', 'words.txt');
+define('PARTS_OF_SPEECH', 'parts.txt');
+
+/**
+ * could sort words in alphabetical order in the input file
+ * and store into the file
+ * randomly choose one word, its part of speech, and its definition
+ *
+ * randomly choose (up to) n other definitions that match that part of
+ * speech
+ * @param $file_name name of the input file
+ */
+function store_words($file_name)
+{
+  $result = array();
+  $index = 0;
+  $lines = file($file_name, FILE_IGNORE_NEW_LINES);
+  while ($index < count($lines))
+  {
+    array_push($result, array($lines[$index]));
+    $index++;
+  }
+  
+  sort($result);
+
+  $line_count = 0;
+  $line = array($result[$line_count][0]);
+  $oneword = $line[0];
+  if (strcmp($oneword, "") != 0)
+  {
+    list($word, $partofspeech, $definition) = explode("\t", $oneword);
+
+    $words = "$word\t$partofspeech\t$definition" . PHP_EOL;
+    file_put_contents(DEFINITION_FILENAME, $words);
+  }
+  else
+  {
+    $line_count++;
+    list($word, $partofspeech, $definition) = explode("\t", $oneword);
+
+    $words = "$word\t$partofspeech\t$definition" . PHP_EOL;
+    file_put_contents(DEFINITION_FILENAME, $words);
+  }
+
+  $line_count++;
+  while ($line_count < count($lines))
+  {
+    $line = array($result[$line_count][0]);
+    $oneword = $line[0];
+    list($word, $partofspeech, $definition) = explode("\t", $oneword);
+
+    $words = "$word\t$partofspeech\t$definition" . PHP_EOL;
+    file_put_contents(DEFINITION_FILENAME, $words,
+      LOCK_EX | FILE_APPEND);
+    $line_count++;
+  }
+}
+
+/**
+ * to delete the input word
+ * @param $array the each line in the words.txt
+ * @param $delete_count the number of lines deleted
+ */
+function delete_word($array, $delete_count)
+{
+  $index = 0;
+  $done = false;
+  $left_sum = count($array) + $delete_count;
+  while ($index < $left_sum && !$done)
+  {
+    if (!array_key_exists($index, $array))
+    {
+      $index++;
+    }
+    else
+    {
+      list($word, $partofspeech, $definition) = explode("\t", $array[$index]);
+      $words = "$word\t$partofspeech\t$definition" . PHP_EOL;
+      file_put_contents(DEFINITION_FILENAME, $words);
+      $done = true;
+      $index++;
+    }
+  }
+  while ($index < $left_sum)
+  {
+    if (!array_key_exists($index, $array))
+    {
+      $index++;
+    }
+    else
+    {
+      list($word, $partofspeech, $definition)
+          = explode("\t", $array[$index]);
+
+      $words = "$word\t$partofspeech\t$definition" . PHP_EOL;
+      file_put_contents(DEFINITION_FILENAME, $words,
+                        LOCK_EX | FILE_APPEND);
+      $index++;
+    }
+  }
+}
+
+/**
+ * could search the part of speech in the input file
+ * @param $file_name  name of the input file
+ * @param $part the part of speech of input word
+ * in the words dictionary.
+ * @return true if the part of speech is found; false otherwise
+ */
+function search_part($file_name, $searched_part)
+{
+  $lines = file($file_name, FILE_IGNORE_NEW_LINES);
+  $index = 0;
+  
+  while ($index < count($lines))
+  {
+    if (strcmp($lines[$index], $searched_part) == 0)
+    {
+      return true;
+    }
+    $index++;
+  }
+  return false;
+}
+
+/**
+ * could search the key word in the input file
+ * @param $file_name  name of the input file
+ * @param $searched_word the input word need to be search
+ * @param $part the part of speech of input word
+ * in the words dictionary.
+ * @return true if the word is found; false otherwise
+ */
+function search_word($file_name, $searched_word, $part)
+{
+  $lines = file($file_name, FILE_IGNORE_NEW_LINES);
+  $index = 0;
+  
+  while ($index < count($lines))
+  {
+    list($word, $partofspeech, $definition) = explode("\t", $lines[$index]);
+
+    $result = array();
+    array_push($result, array($word, $partofspeech, $definition));
+    
+    if (strcmp($result[0][0], $searched_word) == 0
+                && strcmp($result[0][1], $part) == 0)
+    {
+      return true;
+    }
+    $index++;
+  }
+  return false;
+}
+/**
+ * to bool the string is printable or not
+ * @param $string the input string
+ * @return true if the input string is not printable; false is printable 
+ */
+function is_not_printable($string)
+{
+  $index = 0;
+  $done = false;
+  if (!ctype_space($string))
+  {
+    while ($index < strlen($string) && !$done)
+    { 
+      if (ctype_space($string[$index]))
+      {
+        $index++;
+      }
+      else
+      {
+        if (!ctype_graph($string[$index]))
+        {
+          $done = true;
+        }
+        $index++;
+      }
+    }
+  }
+  else
+  {
+    $done = true;
+  }
+  return $done;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Words Manager</title>
+    <meta name="author" content="Jingbo Wang" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="managewords.css">
+  </head>
+  <body>
+    <header>
+      <h1>Manage GRE Vocabulary Words File</h1>
+    </header>
+    <?php
+      $word = "";
+      $part_of_speech = "";
+      $definition = "";
+      $new_part_of_speech = "";
+      $addstatement ="";
+      $deletestatement ="";
+      $addpartofspeech ="";
+      $delete_word_lines = array();
+      
+      if (isset($_POST) &&
+          isset($_POST['words']) &&
+          preg_match('|^[A-Za-z]+$|', $_POST['words']) &&
+          isset($_POST['definition']))
+      {
+        $word = strtolower($_POST['words']);
+        $part_of_speech = $_POST['partofspeech'];
+        $definition = $_POST['definition'];
+        $definition = preg_replace("\n", "", $definition);
+        if (!search_word(DEFINITION_FILENAME, $word, $part_of_speech)
+            && !empty($part_of_speech) 
+            && !is_not_printable($definition))
+        {
+          $definition = htmlspecialchars($definition);
+          $definition = strtolower($definition);
+          $definition = ltrim($definition);
+          $definition = trim($definition);
+          $words = "$word\t$part_of_speech\t$definition" . PHP_EOL;
+
+          $addstatement = "Successfully added!";
+          file_put_contents(DEFINITION_FILENAME, $words,
+                       LOCK_EX | FILE_APPEND);
+          store_words(DEFINITION_FILENAME);
+        }
+        else
+        {
+          $addstatement = "Fail added!";
+        }
+      }
+        
+      $lines = file(DEFINITION_FILENAME, FILE_IGNORE_NEW_LINES);
+      
+      if (isset($_POST) && isset($_POST['delete']))
+      {
+        $delete_word_lines = $_POST['delete'];
+       
+        $index = 0;
+        while ($index < count($delete_word_lines))
+        {
+          $position = $delete_word_lines[$index];
+          unset($lines[$position]);
+          $index++;
+        }
+        delete_word($lines, count($delete_word_lines));
+        $deletestatement = "Successfully delete!";
+      }
+      
+      if (isset($_POST) &&  isset($_POST['newspeech']) &&
+           preg_match('|^[A-Za-z]+$|', $_POST['newspeech']))
+      {
+        $new_part_of_speech = strtolower($_POST['newspeech']);
+        if (!(search_part(PARTS_OF_SPEECH, $new_part_of_speech)))
+        {
+          file_put_contents(PARTS_OF_SPEECH, $new_part_of_speech . PHP_EOL,
+                            LOCK_EX | FILE_APPEND);
+          $addpartofspeech = "Scuccessfully added in the list!";
+        }
+      }
+      else
+      {
+        $addpartofspeech = "Fail added!";
+      }
+    ?>
+    <p id="lastmodified">
+      Last modified: 16 March 2022
+    </p>
+    <h2 id="submit-success-statement">
+      <?php if (!empty($word) && !empty($part_of_speech)
+        && !empty($definition) 
+        && preg_match('|^S|', $addstatement)): ?>
+        <?= $addstatement ?>
+      <?php elseif (!empty($new_part_of_speech)
+                    && preg_match('|^S|', $addpartofspeech)): ?>
+        <?= $addpartofspeech ?>
+      <?php elseif (count($delete_word_lines) != 0
+                    && preg_match('|^S|', $deletestatement)): ?>
+          <?= $deletestatement ?>
+      <?php endif;?>
+    </h2>
+    <form method="post" action="managewords.php">
+      <div id="choose-action">
+        <p class="cb-p">
+          <input type="checkbox" id="add-cb" /><br />
+          <label for="add-cb">Add a Word</label>
+        </p>
+        <p class="cb-p">
+          <input type="checkbox" id="add-pos-cb" /><br />
+          <label for="add-pos-cb">Add part of speech</label>
+        </p>
+        <p class="cb-p">
+          <input type="checkbox" id="del-cb" /><br />
+          <label for="del-cb">Delete Words</label>
+        </p>
+      </div>
+      <hr />
+      <div id="add-wrapper" class="invisible">
+        <section id="add-section">
+          <label for="words">Word:</label>
+          <input type="text" id="words" name="words"  size="10"/>
+          <label for="partofspeech">Parts of speech:</label>
+          <select name="partofspeech" id="partofspeech">
+            <option value="">
+              &mdash; Choose the part of speech &mdash;
+            </option>
+            <?php
+              $lines = file(PARTS_OF_SPEECH, FILE_IGNORE_NEW_LINES);
+              $part_of_speech = null;
+              $line_count = 0;
+              while ($line_count < count($lines)):
+                $part_of_speech = $lines[$line_count];
+            ?>
+            <option value="<?= $part_of_speech ?>">
+              <?= $part_of_speech ?>
+            </option>
+            <?php
+             $line_count++;
+             endwhile;
+            ?>
+          </select>
+          <label for="definition">Definition:</label>
+          <textarea
+            id="definition" name="definition"
+            placeholder="Add definition here" size="50"></textarea>
+          <button type="submit" id="add-submit">Add</button>
+        </section>
+      </div>
+      <div id="add-pos-wrapper" class="invisible">
+        <section id="add-pos-section">
+          <p>
+            Part of speech list:
+            <em id="partofspeech-list">
+              <?php
+              $index = 0;
+              $lines = file(PARTS_OF_SPEECH, FILE_IGNORE_NEW_LINES);
+              while ($index < count($lines)):
+                $part_of_speech = $lines[$index];
+                ?>
+                <?php  if ($index == count($lines) - 1):?>
+                <?= $part_of_speech ?>
+              <?php else: ?>
+                <?= $part_of_speech ?>,
+              <?php endif; ?>
+                <?php
+                $index++;
+              endwhile;
+              ?>
+            </em>
+          </p>
+          <p>
+            <label for="newspeech">
+              Add new part of speech(Do not exist in list):
+            </label>
+            <input type="text" id="newspeech" name="newspeech" size="20"/>
+          </p>
+          <button type="submit" id="add-pos-submit">
+            Add
+          </button>
+        </section>
+      </div>
+      <div id="del-wrapper" class="invisible">
+        <section id="del-section">
+          <button type="submit" id="del-submit">Delete</button>
+          <dl id="word-list">
+            <?php
+              $line_count = 0;
+              $lines = file(DEFINITION_FILENAME, FILE_IGNORE_NEW_LINES);
+              $wordlist = array();
+              while ($line_count < count($lines))
+              {
+                $wordlist[] = explode("\t", rtrim($lines[$line_count]));
+                $line_count++;
+              }
+              $index = 0;
+              foreach ($wordlist as $row):
+            ?>
+            <dt>
+              <input type="checkbox" name="delete[]" class="wordslist"
+                     id="<?= $index ?>" value="<?= $index ?>" />
+              <label id="id-<?= $index ?>" for="<?= $index ?>">
+                <?= $row[0] ?> :
+                <span class="partofspeech"><?= $row[1] ?></span>
+              </label>
+            </dt>
+            <dd class="definition">
+              <?= $row[2] ?>
+            </dd>
+            <?php
+              $index++;
+              endforeach;
+            ?>
+          </dl>
+        </section>
+      </div>
+      <h2 id="statement">
+        Select one to add a word, add part of speech or delete words!
+      </h2>
+    </form>
+    <script src="managewords.js"></script>
+  </body>
+</html>
