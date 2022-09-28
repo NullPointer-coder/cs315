2764b1024575d523419b7a15a5dc8b13ee7a24d9
NullPointer-coder <wangjb12018@gmail.com>
2022-09-27 19:59:25 -0500

    nothing

--- Assignment429/429/table.sql
+++ Assignment429/429/table.sql
@@ -3,18 +3,18 @@ drop table if exists password;
 
 create table password
 (
-	id int unsigned not null auto_increment,
-	password  longtext not null,
-	primary key(id)
+  id int unsigned not null auto_increment,
+  password  VARCHAR(255) not null,
+  primary key(id)
 ) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_0900_ai_ci;
 
 create table user
 (
-	id int unsigned not null auto_increment,
-	user_name varchar(255) not null,
+  id int unsigned not null auto_increment,
+  user_name varchar(255) not null,
   display_name varchar(255) not null,
   manager boolean not null,
-	password_id int unsigned not null,
-	primary key(id),
-	constraint password_fk foreign key(password_id) references password(id)
+  password_id int unsigned not null,
+  primary key(id),
+  constraint password_fk foreign key(password_id) references password(id)
 ) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_0900_ai_ci;
